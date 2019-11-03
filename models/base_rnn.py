One-hot encoding and fix gradients for unconditional

models/base_rnn.py
@@ -36,18 +36,18 @@ class BaseRNN():
                                     use_bias=True,
                                     bias_initializer='zeros')
 
-    # expands dims of coordinates for gaussian and loss calculations
-    def expand_dims(self, inputs, axis, num_dims):
-        return tf.concat([tf.expand_dims(inputs, axis) for _ in range(num_dims)], axis)
+    # expands dims of input coordinates for gaussian and loss calculations
+    def expand_input_dims(self, inputs):
+        return tf.concat([inputs for _ in range(self.num_mixtures)], -1)
 
     # Equations (18 - 23)
     def output_vector(self, outputs):
-        e_hat = outputs[:, :, 0]
+        e_hat = outputs[:, :, 0:1]
         pi_hat, mu_hat1, mu_hat2, sigma_hat1, sigma_hat2, rho_hat = tf.split(outputs[:, :, 1:], 6, -1)
 
         # calculate actual values
-        end_stroke = tf.math.sigmoid(e_hat)
-        mixture_weight = tf.math.softmax(pi_hat, axis=-1)
+        end_stroke = tf.math.sigmoid(-e_hat)
+        mixture_weight = tf.nn.softmax(pi_hat, axis=-1)
         mean1 = mu_hat1
         mean2 = mu_hat2
         stddev1 = tf.math.exp(sigma_hat1)
@@ -78,8 +78,9 @@ class BaseRNN():
 
     def train(self, epochs=50, batch_size=64, learning_rate=0.0001, epochs_per_save=10):
         self.build_model(batch_size=batch_size)
-        self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=0.95,
-                                                     momentum=0.9, epsilon=0.0001)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        #self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=learning_rate, rho=0.95,
+        #                                             momentum=0.9, epsilon=0.0001)
         self.model.summary()
         dataloader = Dataloader(batch_size=batch_size)
 
@@ -106,16 +107,15 @@ class BaseRNN():
             print('Finished Epoch {} with training loss {:.6f} and validation loss {:.6f}'.format(
                   epoch + 1, self.train_loss.result(), self.validation_loss.result()))
 
-            if self.validation_loss.result() < best_loss:
+            if self.train_loss.result() < best_loss:
                 self.save('_best')
-                best_loss = self.validation_loss.result()
+                best_loss = self.train_loss.result()
                 self.generate(filepath='samples/{}/generated_best.jpeg'.format(self.name))
-                self.build_model()
+                self.build_model('_best')
 
             if epoch % epochs_per_save == 0:
                 self.save('_{}'.format(epoch))
-                self.generate(filepath='samples/{}/generated_{}.jpeg'.format(self.name, epoch))
-                self.build_model()
+                self.build_model('_{}'.format(epoch))
 
             # log metrics
             with train_summary_writer.as_default():

models/unconditional_rnn.py
@@ -13,7 +13,7 @@ class UnconditionalRNN(BaseRNN):
     def __init__(self, *args, **kwargs):
         super().__init__('unconditional', *args, **kwargs)
 
-    def build_model(self, batch_size=None, stateful=False):
+    def build_model(self, load_suffix='_best', batch_size=None, stateful=False):
         inputs = tf.keras.Input(shape=(None, self.input_size), batch_size=batch_size)
 
         lstm_1 = self.lstm_layer((batch_size, None, self.input_size), stateful=stateful)(inputs)
@@ -27,15 +27,15 @@ class UnconditionalRNN(BaseRNN):
                                              input_shape=(self.num_layers * self.num_cells,))(skip)
 
         self.model = tf.keras.Model(inputs=inputs, outputs=outputs)
-        self.load('_best')
+        self.load(load_suffix)
 
     # Equation (26)
     def loss(self, x, y, input_end, mean1, mean2, stddev1, stddev2, correl, mixture_weight, end_stroke):
-        epsilon = 1e-10 # required for logs to not be NaN when value is zero
-        gaussian = mixture_weight * self.bivariate_gaussian(self.expand_dims(x, -1, self.num_mixtures),
-                                                            self.expand_dims(y, -1, self.num_mixtures),
+        epsilon = 1e-8 # required for logs to not be NaN when value is zero
+        gaussian = mixture_weight * self.bivariate_gaussian(self.expand_input_dims(x),
+                                                            self.expand_input_dims(y),
                                                             stddev1, stddev2, mean1, mean2, correl)
-        gaussian_loss = tf.reduce_sum(gaussian, axis=-1)
+        gaussian_loss = tf.expand_dims(tf.reduce_sum(gaussian, axis=-1), axis=-1)
         gaussian_loss = tf.math.log(tf.maximum(gaussian_loss, epsilon))
         bernoulli_loss = tf.where(tf.math.equal(tf.ones_like(input_end), input_end), end_stroke, 1 - end_stroke)
         bernoulli_loss = tf.math.log(tf.maximum(bernoulli_loss, epsilon))
@@ -44,10 +44,15 @@ class UnconditionalRNN(BaseRNN):
     @tf.function
     def train_step(self, inputs, update_gradients=True):
         with tf.GradientTape() as tape:
+            tape.watch(inputs)
             outputs = self.model(inputs)
             end_stroke, mixture_weight, mean1, mean2, stddev1, stddev2, correl = self.output_vector(outputs)
-            loss = tf.reduce_mean(self.loss(inputs[:,:,1], inputs[:,:,2], inputs[:,:,0], mean1, mean2,
-                                                  stddev1, stddev2, correl, mixture_weight, end_stroke)) / 800
+
+            input_end_stroke = tf.expand_dims(tf.gather(inputs, 0, axis=-1), axis=-1)
+            x = tf.expand_dims(tf.gather(inputs, 1, axis=-1), axis=-1)
+            y = tf.expand_dims(tf.gather(inputs, 2, axis=-1), axis=-1)
+            loss = tf.reduce_mean(self.loss(x, y, input_end_stroke, mean1, mean2,
+                                            stddev1, stddev2, correl, mixture_weight, end_stroke))
 
         trainable_vars = self.model.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
@@ -70,11 +75,12 @@ class UnconditionalRNN(BaseRNN):
             mixture_idx = mixture_dist.sample(seed=seed)
 
             # retrieve correct distribution values from mixture
-            mean1 = mean1[0,0,mixture_idx]
-            mean2 = mean2[0,0,mixture_idx]
-            stddev1 = stddev1[0,0,mixture_idx]
-            stddev2 = stddev2[0,0,mixture_idx]
-            correl = correl[0,0,mixture_idx]
+            mean1 = tf.gather(mean1, mixture_idx, axis=-1)
+            mean2 = tf.gather(mean2, mixture_idx, axis=-1)
+            stddev1 = tf.gather(stddev1, mixture_idx, axis=-1)
+            stddev2 = tf.gather(stddev2, mixture_idx, axis=-1)
+            correl = tf.gather(correl, mixture_idx, axis=-1)
+            print("GEN: ", end_stroke, mixture_weight, mean1, stddev1, correl)
 
             # sample for x, y offsets
             cov_matrix = [[stddev1 * stddev1, correl * stddev1 * stddev2],
@@ -85,7 +91,7 @@ class UnconditionalRNN(BaseRNN):
 
             # sample for end of stroke
             bernoulli = tfp.distributions.Bernoulli(probs=end_stroke)
-            end_cur_stroke = bernoulli.sample(1, seed=seed)
+            end_cur_stroke = bernoulli.sample(seed=seed)
 
             sample[0,i+1] = [end_cur_stroke, x, y]
             inputs = outputs

utils/data.py
@@ -16,7 +16,9 @@ class Dataloader:
         self.buffer_size = buffer_size
         self.drop_remainder = drop_remainder
         self.max_sequence_length = max_sequence_length
+        self.init_datasets()
 
+    def init_datasets(self):
         base_path = Path(__file__).parent
         strokes_path = str((base_path / "../data/data.npy").resolve())
         sentences_path = str((base_path / "../data/sentences.txt").resolve())
@@ -30,6 +32,15 @@ class Dataloader:
         self.valid_strokes = []
         self.valid_sentences = []
 
+        # removed any characters belonging in '0123456789\n+/#():;'
+        characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,"\'!?'
+        self.char_to_index = {}
+        self.num_characters = len(characters) + 1
+
+        # first index reserved for unknown
+        for idx, char in enumerate(characters):
+            self.char_to_index[char] = idx + 1
+
         # filter out long sequences and use them as validation set
         for stroke, sentence in zip(strokes, sentences):
             if len(stroke) > self.max_sequence_length:
@@ -39,35 +50,55 @@ class Dataloader:
                 self.train_strokes.append(stroke)
                 self.train_sentences.append(sentence)
 
-        self.train_strokes = tf.keras.preprocessing.sequence.pad_sequences(self.train_strokes,
-                                                                           dtype='float32',
-                                                                           padding='post',
-                                                                           value=0.0)
-        self.valid_strokes = tf.keras.preprocessing.sequence.pad_sequences(self.valid_strokes,
-                                                                           dtype='float32',
-                                                                           padding='post',
-                                                                           value=0.0)
+        self.max_train_sentence_length = max([len(sentence) for sentence in self.train_sentences])
+        self.max_valid_sentence_length = max([len(sentence) for sentence in self.valid_sentences])
+        self.num_train_batches = np.math.ceil(len(self.train_strokes) // self.batch_size)
+        self.num_valid_batches = np.math.ceil(len(self.valid_strokes) // self.batch_size)
 
-        self.train_dataset = tf.data.Dataset.from_tensor_slices(self.train_strokes)
-        self.train_dataset = self.train_dataset.batch(self.batch_size, drop_remainder=self.drop_remainder)
+        # build strokes datasets
+        self.train_strokes = tf.keras.preprocessing.sequence.pad_sequences(self.train_strokes, dtype='float32',
+                                                                           padding='post', value=0.0)
+        self.valid_strokes = tf.keras.preprocessing.sequence.pad_sequences(self.valid_strokes, dtype='float32',
+                                                                           padding='post', value=0.0)
+        
+        self.train_strokes = tf.data.Dataset.from_tensor_slices(self.train_strokes)
+        self.train_strokes = self.train_strokes.batch(self.batch_size, drop_remainder=self.drop_remainder)
 
-        self.valid_dataset = tf.data.Dataset.from_tensor_slices(self.valid_strokes)
-        self.valid_dataset = self.valid_dataset.batch(self.batch_size, drop_remainder=self.drop_remainder)
+        self.valid_strokes = tf.data.Dataset.from_tensor_slices(self.valid_strokes)
+        self.valid_strokes = self.valid_strokes.batch(self.batch_size, drop_remainder=self.drop_remainder)
 
-        self.sentence_train_dataset = tf.data.Dataset.from_tensor_slices(self.train_sentences)
-        self.sentence_train_dataset = self.sentence_train_dataset.batch(self.batch_size,
-                                                                        drop_remainder=self.drop_remainder)
+        # build sentences datasets
+        self.train_sentences = [self.one_hot_encode(sentence, self.max_train_sentence_length)
+                                for sentence in self.train_sentences]
+        self.valid_sentences = [self.one_hot_encode(sentence, self.max_valid_sentence_length)
+                                for sentence in self.valid_sentences]
 
-        self.sentence_valid_dataset = tf.data.Dataset.from_tensor_slices(self.valid_sentences)
-        self.sentence_valid_dataset = self.sentence_valid_dataset.batch(self.batch_size,
-                                                                         drop_remainder=self.drop_remainder)
+        self.train_sentences = tf.data.Dataset.from_tensor_slices(self.train_sentences)
+        self.train_sentences = self.train_sentences.batch(self.batch_size,
+                                                                 drop_remainder=self.drop_remainder)
 
+        self.valid_sentences = tf.data.Dataset.from_tensor_slices(self.valid_sentences)
+        self.valid_sentences = self.valid_sentences.batch(self.batch_size,
+                                                                 drop_remainder=self.drop_remainder)
 
-    def load_datasets(self):
+    def one_hot_encode(self, sentence, max_length):
+        one_hot = np.zeros((max_length, self.num_characters), dtype='float32')
+        for idx, char in enumerate(sentence):
+            if char in self.char_to_index:
+                one_hot[idx][self.char_to_index[char]] = 1.0
+            else:
+                one_hot[idx][0] = 1.0
+        return one_hot
+
+    # loads datasets for training
+    def load_datasets(self, include_sentences=False):
+        if not include_sentences:
+            self.train_dataset = self.train_strokes
+            self.valid_dataset = self.valid_strokes
+        else:
+            self.train_dataset = tf.data.Dataset.zip((self.train_strokes, self.train_sentences))
+            self.valid_dataset = tf.data.Dataset.zip((self.valid_strokes, self.valid_sentences))
+
+        # shuffle data
         self.train_dataset = self.train_dataset.shuffle(buffer_size=self.buffer_size)
-        self.num_train_batches = len(self.train_strokes) // self.batch_size
-
-        valid_dataset = tf.data.Dataset.from_tensor_slices(self.valid_strokes)
-        valid_dataset = valid_dataset.batch(self.batch_size, drop_remainder=self.drop_remainder)
-        self.valid_dataset = valid_dataset.shuffle(buffer_size=self.buffer_size)
-        self.num_valid_batches = len(self.valid_strokes) // self.batch_size
+        self.valid_dataset = self.valid_dataset.shuffle(buffer_size=self.buffer_size)